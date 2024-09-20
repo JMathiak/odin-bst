96b87ea38d709f4b3ade698249a28f46c65a5f40
JMathiak <jmathiak32@yahoo.com>
2024-09-20 18:44:57 -0500

    wrote buildTree function

---
+++ Tree.js
@@ -0,0 +1,78 @@
+import { Node } from "./Node.js";
+
+class Tree{
+    constructor(arr){
+        arr = [...new Set(arr)]
+        this.data = mergeSort(arr)
+        this.root = this.buildTree(this.data)
+
+    }
+
+    buildTree(arr){
+        if(arr.length <= 1) return null;
+
+        let mid = Math.floor(arr.length / 2)
+        let root = new Node(arr[mid])
+        root.left = this.buildTree(arr.slice(0, mid))
+        root.right = this.buildTree(arr.slice(mid))
+
+        return root;
+    }
+}
+
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  };
+
+  function merge(lArr, rArr){
+    let sorted = []
+    while(lArr.length && rArr.length){
+        if(lArr[0] < rArr[0])
+        {
+            sorted.push(lArr.shift())
+            
+    
+        }else{
+            sorted.push(rArr.shift())
+           
+            
+        }
+    }
+  
+    return [...sorted, ...lArr, ...rArr]
+}
+
+function mergeSort(arr)
+{
+        if (arr.length <= 1)
+        {
+            return arr
+        }
+        let mid = Math.floor(arr.length / 2)
+        let left = mergeSort(arr.slice(0, mid))
+        let right = mergeSort(arr.slice(mid))
+        return merge(left, right)
+    
+    
+   
+
+}
+
+let arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+let test = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+prettyPrint(test.root)
+
+for(let i = 0; i < test.data.length; i++)
+{
+    console.log(test.data[i])
+}
